ef1435355d3de93c52f47bf0435d6aa62ef3bcc7
ArthurChome <Arthur.Vincent.Chome@vub.be>
2019-09-20 17:51:46 +0200

    Added Follower script for path creation

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,28 @@
+﻿/*
+ To be used together with the free Unity asset Path Creator
+ https://www.youtube.com/watch?v=saAQNRSYU9k
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using PathCreation;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Nothing
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTravelled += speed + Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+    }
+}
